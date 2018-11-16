127ee42fb0d8362a5e4ff47e3fd5608101565e34
Rakesh <rakesh.cheekatimala@viseo.com>
2018-11-16 22:11:04 +0800

    :tada: commit for util function that returns the organized data for the issues array based on keyname 'type'

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,14 @@ export const findIssueById = (issues, id) => {
   }
   return null;
 };
+
+// This function iterates the input which is an array based on keyName and returns the count of each label
+// for ex: issues arr of size 5 with keyName as 'type' returns { close: 2 , open:1, reopen:2} based on the input
+export const organizeChartData = (input,keyName) => {
+  let result = {};
+  for(let i=0;i<input.length;i++) {
+    let type=input[i][keyName];
+    result.hasOwnProperty(type)?result[type]=++result[type]: result[type]=1;
+  }
+  return result;
+}
