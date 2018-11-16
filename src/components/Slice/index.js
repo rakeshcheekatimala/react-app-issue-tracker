635953a9692244bd38f1bb0930e7d6797d0c4a3c
Rakesh <rakesh.cheekatimala@viseo.com>
2018-11-16 22:31:08 +0800

    :tada: Commit for Rendering the Slice for Pie Chart

---
+++ src/components/Slice/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import * as d3 from "d3"
+
+function translate(x, y) {
+  return `translate(${x}, ${y})`;
+}
+
+const Slice = props =>{
+  let {pie,innerRadius,outerRadius,textlabels} =props;
+  let {data} = props;
+  let arc = d3.arc()
+  .innerRadius(innerRadius)
+  .outerRadius(outerRadius)
+  //let interpolate = d3.interpolateRgb('#eaaf79','#bc3358');
+  let interpolate = d3.schemeCategory10
+  return pie.map((slice,index)=>{
+    let sliceColor = interpolate[index]
+    console.log("slice value is",slice,...arc.centroid(slice));
+    return (<g key={index}>
+      <path d ={arc(slice)} fill={sliceColor}/>
+      <text dy=".35em" transform={translate(...arc.centroid(slice))} style={{"textAnchor":"middle","fill":"#fff","fontSize":"18px","fontWeight":"bold"}}>
+         {data[index]}-{textlabels[index]}
+       </text>
+
+      </g>)
+  })
+}
+
+export default Slice;
