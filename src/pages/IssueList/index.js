e6d9d996d26f910b977f8ba6c3302655292e48a3
Rakesh <rakesh.cheekatimala@viseo.com>
2018-11-19 22:36:55 +0800

    :wave: Adding the Feature of DragDrop Functionality to move the cards.

--- src/pages/IssueList/index.js
+++ src/pages/IssueList/index.js
@@ -1,9 +1,10 @@
 import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { getIssues, setIssue } from "../../store/actions/issue";
-import { findIssueById } from "../../utils";
+import { getIssues, setIssue, updateIssue } from "../../store/actions/issue";
+import { findIssueById, groupIssues } from "../../utils";
 import "./styles.css";
+const SECTIONS = ["open", "inprogress", "reopen", "close"];
 
 class IssueList extends PureComponent {
   componentDidMount() {
@@ -12,16 +13,49 @@ class IssueList extends PureComponent {
   renderEmptyMessage = () => {
     return <div> No Issues Created Yet</div>;
   };
+
   onEdit = id => {
     const { issues, editIssue } = this.props;
     const issue = findIssueById(issues, id);
     editIssue(issue);
   };
 
+  onDragOver = e => {
+    e.preventDefault();
+  };
+
+  onDrop = (e, section) => {
+    let id = e.dataTransfer.getData("id");
+    console.log(
+      "the drop status is to be changed",
+      "for the element",
+      id,
+      section
+    );
+    let { issues, dragUpdateIssue } = this.props;
+
+    let newObj = findIssueById(issues, id);
+    newObj.type = section;
+
+    console.log("the updated issues arrary is", issues);
+    dragUpdateIssue(newObj);
+  };
+
+  onDragStart = (e, id) => {
+    console.log("inside onDragStart", id, "the section");
+    e.dataTransfer.setData("id", id);
+    //store the element that is being dragged
+  };
+
   renderCards = issues => {
     return issues.map(issue => {
       return (
-        <div className="card" key={issue.id}>
+        <div
+          className="card"
+          key={issue.id}
+          draggable
+          onDragStart={e => this.onDragStart(e, issue.id)}
+        >
           <div className="content">
             <div className="header">
               {issue.title}
@@ -29,7 +63,6 @@ class IssueList extends PureComponent {
             </div>
             <div>{issue.description}</div>
           </div>
-
           <div className="action-group">
             <button className="edit-btn" onClick={() => this.onEdit(issue.id)}>
               Edit
@@ -39,15 +72,38 @@ class IssueList extends PureComponent {
       );
     });
   };
+
+  renderSections = (sections, result) => {
+    return sections.map((section, index) => {
+      return (
+        <div
+          className="issues"
+          key={index}
+          onDragOver={e => this.onDragOver(e)}
+          onDrop={e => this.onDrop(e, section)}
+        >
+          <h1> {section.toUpperCase()} ISSUES </h1>
+          {result[section] ? (
+            <div className="cards">{this.renderCards(result[section])}</div>
+          ) : null}
+        </div>
+      );
+    });
+  };
+
   render() {
     const { issues } = this.props;
+
+    let result = null;
+    if (issues) {
+      result = groupIssues(issues);
+    }
     return (
       <div className="issue-list-container">
         <h1>IssueList </h1>
-        <div className="cards">
-          {issues.length === 0
-            ? this.renderEmptyMessage()
-            : this.renderCards(issues)}
+
+        <div className="card-layout">
+          {SECTIONS ? this.renderSections(SECTIONS, result) : null}
         </div>
       </div>
     );
@@ -62,7 +118,8 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return {
     loadIssues: () => getIssues(dispatch),
-    editIssue: issue => setIssue(dispatch, issue)
+    editIssue: issue => setIssue(dispatch, issue),
+    dragUpdateIssue: issue => updateIssue(dispatch, issue)
   };
 }
 
